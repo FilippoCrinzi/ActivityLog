f21a5c83b92551666af18ea0449e8686b92b1363
FilippoCrinzi <103121465+FilippoCrinzi@users.noreply.github.com>
2022-10-06 17:06:31 +0200

    three

--- Log.cpp
+++ Log.cpp
@@ -5,7 +5,17 @@
 #include "Log.h"
 #include "Date.h"
 
-void Log::AddActivity(const Date d,Activity a) {
+void Log::AddActivity(Date& d,Activity a) {
+    std::map<Date, std::list<Activity>>::iterator itr;
+    bool find=false;
+    for(itr = Register.begin(); itr != Register.end(); itr++){
+        if(itr->first == d)
+        itr->second.push_back(a);
+        find=true;
+    }
+    if(!find){
+       // Register.insert(std::make_pair(d, new std:: list<Activity>));
+    }
     }
 
 void Log::ReadActivity(Date giorno) {
